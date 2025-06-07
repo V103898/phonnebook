640f1836eb55683b83857653fe5dfa834fb2074b
CTACOB <star2203@yandex.ru>
2025-06-07 17:26:42 +0300

    Stub and tests for Add.

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,26 @@
 package ru.netology;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.ArrayList;
 
 public class PhoneBook {
-}
+
+    private final Map<String, String> nameToNumber = new HashMap<>();
+    private final Map<String, String> numberToName = new HashMap<>();
+    private final TreeSet<String> sortedNames = new TreeSet<>();
+
+
+    public int add(String name, String number) {
+        if (!nameToNumber.containsKey(name)) {
+            nameToNumber.put(name, number);
+            numberToName.put(number, name);
+            sortedNames.add(name);
+        }
+        return 0;
+    }
+
+
+    }
+
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.Test;
+import ru.netology.PhoneBook;
+
+import static org.junit.jupiter.api.Assertions.*;
 public class PhoneBookTest {
+    @Test
+    public void testAddSingleContact() {
+        PhoneBook phoneBook = new PhoneBook();
+        int count = phoneBook.add("AHHA", "123");
+        assertEquals(1, count);
+    }
+
+    @Test
+    public void testAddDuplicateName() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("AHHA", "123");
+        int count = phoneBook.add("AHHA", "456"); // дубликат имени
+        assertEquals(1, count); // размер не должен измениться
+    }
 }
